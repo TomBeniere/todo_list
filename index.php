1c6a6affd7c4ac0613d05b3ee788bffe4322724f
mthdht <mthdht@gmail.com>
2019-11-14 09:53:46 +0100

    remove sol

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php
-function escape($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-
-    return $data;
-}
-
-try{
-    $pdo = new PDO("mysql:host=localhost;dbname=sql_php", 'root', 'root');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $req = $pdo->query('SELECT * FROM todos ORDER BY id DESC');
-}
-catch(PDOException $e){
-    echo "Erreur : " . $e->getMessage();
-}
+// Code here...
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,37 +19,32 @@ catch(PDOException $e){
     <main class="text-gray-800">
        <section class="md:w-2/3 lg:w-1/3 mx-auto mt-4">
             <div class="add text-center p-2">
-                <form action="traitement.php" method="post">
+                <form>
                     <input type="text" name="todo" value="" class="border p-2 rounded" placeholder="Nouvel item">
                     <input type="submit" value="Ajouter" class="py-2 px-4 rounded bg-green-500 text-white">
                 </form>
             </div>
             <table class="w-full mt-2">
 
-            <?php
-                while($data = $req->fetch()) {
-            ?>
+            
                 <tr class="border-t-2">
-                    <td class="px-4 todo-item p-2"><?php echo escape($data['todo']);?></td>
+                    <td class="px-4 todo-item p-2"><?php echo "Une chose a faire";?></td>
                     <td class="hidden todo-input p-2">
                         <form action="traitement.php" method="post" class="text-center">
-                            <input type="text" name="update" class="border p-2 rounded" value="<?php echo escape($data['todo']);?>">
-                            <input type="hidden" name="id" value="<?php echo escape($data['id']);?>">
+                            <input type="text" name="update" class="border p-2 rounded" value="<?php echo "Une chose a faire";?>">
+                            <input type="hidden" name="id" value="<?php "1";?>">
                             <input type="submit" value="Changer" class="py-2 px-4 rounded bg-green-500 text-white"> 
                         </form>  
                     </td>
                     <td class="todo-actions text-center p-2 flex justify-center">
                         <button class="p-2 rounded bg-yellow-500 todo-update mr-4"><i class="fas fa-pen text-white"></i></button>
                         <form action="traitement.php" method="post">
-                            <input type="hidden" name="delete" value="<?php echo escape($data['id']);?>">
+                            <input type="hidden" name="delete" value="<?php echo "Une chose a faire";?>">
                             <button class="p-2 rounded bg-red-500"><i class="fas fa-trash text-white"></i></button>
                         </form>
                     </td>
                 </tr>
-                <?php 
-                }
-                $req->closeCursor();
-                ?>
+                
             </table>
        </section>
     </main>
